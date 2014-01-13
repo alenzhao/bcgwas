be9fedfdb944e45c60cf6540f88ef5c15d3b8708
Nathanael Fillmore <github@nate-fillmore.com>
2014-01-13 13:36:02 -0600

    Encode chromosome; avoid RSQLite.
    - That is, I changed the encoding of the chromosome to be consistent in
      the expr and geno tables.
    
    - I also changed the database creation tool to just output a text file
      of SQL commands, which are then loaded using the sqlite3 program. This
      seems to be significantly faster than using RSQLite (perhaps it isn't
      possible to turn off autocommit in RSQLite?).

--- make_database.R
+++ make_database.R
@@ -1,11 +1,19 @@
-require("RSQLite")
 require("plyr")
 
 # This script makes an sqlite database from the CSV files provided by Fred.
 #
 # Usage:
-#   datadir = "/tier2/deweylab/scratch/nathanae/mn/bcgwas"
-#   source("make_database.R")
+#
+#   In R:
+#   > datadir = "/tier2/deweylab/scratch/nathanae/mn/bcgwas"
+#   > source("make_database.R")
+#
+#   Then, at the command line:
+#   $ cd /tier2/deweylab/scratch/nathanae/mn/bcgwas
+#   $ time sqlite3 bcgwas.db < bcgwas.sql
+#
+#   Optionally:
+#   $ rm bcgwas.sql
 stopifnot(exists("datadir"))
 
 cat("Load data.\n")
@@ -17,12 +25,10 @@ stopifnot(all(names(expr) == names(geno)))
 stopifnot(all(rownames(geno) == snp_ann$rs.id))
 stopifnot(all(rownames(expr) == as.character(trait_ann$TranscriptClusterID)))
 
-cat("Create database and establish a connection to it.\n")
-dbname = sprintf("%s/bcgwas.db", datadir)
-if (file.exists(dbname))
-  file.remove(dbname)
-driver = dbDriver("SQLite")
-conn   = dbConnect(driver, dbname)
+cat("Open SQL file.\n")
+fo = file(sprintf("%s/bcgwas.sql", datadir), open="w")
+sql = function(x) { cat(sprintf("%s;\n", x), file=fo); }
+sql("begin transaction")
 
 cat("Create expr table.\n")
 expr_patient_cols = paste0(sprintf("%s real", names(expr)), collapse=", ")
@@ -42,20 +48,18 @@ create_expr = sprintf("
     probeset_type       text,
     %s
   )", expr_patient_cols)
-dbBeginTransaction(conn)
-dbGetQuery(conn, create_expr)
-dbGetQuery(conn, "create index expr_gene_name           on expr (gene_name)")
-dbGetQuery(conn, "create index expr_gene_symbol         on expr (gene_symbol)")
-dbGetQuery(conn, "create index expr_gene_accession      on expr (gene_accession)")
-dbGetQuery(conn, "create index expr_entrez_id           on expr (entrez_id)")
-dbGetQuery(conn, "create index expr_chromosome          on expr (chromosome)")
-dbGetQuery(conn, "create index expr_cytoband            on expr (cytoband)")
-dbGetQuery(conn, "create index expr_start               on expr (start)")
-dbGetQuery(conn, "create index expr_stop                on expr (stop)")
-dbGetQuery(conn, "create index expr_strand              on expr (strand)")
-dbGetQuery(conn, "create index expr_cross_hybridization on expr (cross_hybridization)")
-dbGetQuery(conn, "create index expr_probeset_type       on expr (probeset_type)")
-dbCommit(conn)
+sql(create_expr)
+sql("create index expr_gene_name           on expr (gene_name)")
+sql("create index expr_gene_symbol         on expr (gene_symbol)")
+sql("create index expr_gene_accession      on expr (gene_accession)")
+sql("create index expr_entrez_id           on expr (entrez_id)")
+sql("create index expr_chromosome          on expr (chromosome)")
+sql("create index expr_cytoband            on expr (cytoband)")
+sql("create index expr_start               on expr (start)")
+sql("create index expr_stop                on expr (stop)")
+sql("create index expr_strand              on expr (strand)")
+sql("create index expr_cross_hybridization on expr (cross_hybridization)")
+sql("create index expr_probeset_type       on expr (probeset_type)")
 
 cat("Create geno table.\n")
 geno_patient_cols = paste0(sprintf("%s integer", names(expr)), collapse=", ")
@@ -64,27 +68,28 @@ create_geno = sprintf("
     snp_id      integer primary key autoincrement,
     snp_name    text,
     position    integer,
-    chromosome  integer,
+    chromosome  text,
     missing_n1  real,
     missing_n2  real,
     %s
   )", geno_patient_cols)
-dbBeginTransaction(conn)
-dbGetQuery(conn, create_geno)
-dbGetQuery(conn, "create index geno_snp_name   on geno (snp_name)")
-dbGetQuery(conn, "create index geno_position   on geno (position)")
-dbGetQuery(conn, "create index geno_chromosome on geno (chromosome)")
-dbCommit(conn)
+sql(create_geno)
+sql("create index geno_snp_name   on geno (snp_name)")
+sql("create index geno_position   on geno (position)")
+sql("create index geno_chromosome on geno (chromosome)")
 
 cat("Prepare to insert expr data.\n")
 meta_cols = "gene_id, gene_name, gene_symbol, gene_accession, entrez_id, chromosome, cytoband, start, stop, strand, cross_hybridization, probeset_type"
+expr_chromosome = trait_ann$Chromosome
+expr_chromosome = gsub("chr", "", expr_chromosome)
+expr_chromosome = gsub("_random", "", expr_chromosome)
 meta_data = matrix(c(
   sprintf("\"%s\"", trait_ann$TranscriptClusterID), # These sprintf convert
   sprintf("\"%s\"", trait_ann$GeneName),            # NA to "\"NA\"" or "NA".
   sprintf("\"%s\"", trait_ann$GeneSymbol),
   sprintf("\"%s\"", trait_ann$GeneAccession),
   sprintf("%d",     trait_ann$EntrezID),
-  sprintf("\"%s\"", trait_ann$Chromosome),
+  sprintf("\"%s\"", expr_chromosome),
   sprintf("\"%s\"", trait_ann$Cytoband),
   sprintf("%d",     trait_ann$Start),
   sprintf("%d",     trait_ann$Stop),
@@ -101,18 +106,21 @@ stopifnot(all(trait_ann$TranscriptClusterID == names(patient_data)))
 stmts = sprintf("insert into expr (%s, %s) values (%s, %s)", meta_cols, patient_cols, meta_data, patient_data)
 
 cat("Insert expr data.\n")
-dbBeginTransaction(conn)
-llply(stmts, function(stmt) { dbGetQuery(conn, stmt) }, .progress="text")
-dbCommit(conn)
+llply(stmts, function(stmt) { sql(stmt) }, .progress="text")
 
 cat("Prepare to insert geno data.\n")
 stopifnot(all(snp_ann$rs.id == rownames(geno)))
 meta_cols = "snp_name, position, chromosome, missing_n1, missing_n2"
 #meta_data = sprintf("\"%s\", %d, %d, %.16f, %.16f", snp_ann$rs.id, snp_ann$position, snp_ann$chromosome, snp_ann$missing.n1, snp_ann$missing.n2)
+snp_chromosome = sprintf("%d", snp_ann$chromosome)
+snp_chromosome = gsub("23", "X", snp_chromosome)
+snp_chromosome = gsub("24", "Y", snp_chromosome)
+snp_chromosome = gsub("25", "XY", snp_chromosome)
+snp_chromosome = gsub("26", "M", snp_chromosome)
 meta_data = matrix(c(
   sprintf("\"%s\"", snp_ann$rs.id),
   sprintf("%d",     snp_ann$position),
-  sprintf("%d",     snp_ann$chromosome),
+  sprintf("\"%s\"", snp_chromosome),
   sprintf("%.16f",  snp_ann$missing.n1),
   sprintf("%.16f",  snp_ann$missing.n2)
 ), nrow(snp_ann))
@@ -126,9 +134,8 @@ patient_data = apply(patient_data, 1, paste0, collapse=", ")
 stmts = sprintf("insert into geno (%s, %s) values (%s, %s)", meta_cols, patient_cols, meta_data, patient_data)
 
 cat("Insert geno data.\n")
-dbBeginTransaction(conn)
-llply(stmts, function(stmt) { dbGetQuery(conn, stmt) }, .progress="text")
-dbCommit(conn)
+llply(stmts, function(stmt) { sql(stmt) }, .progress="text")
 
-cat("Cleanup connections.\n")
-dbDisconnect(conn)
+cat("Close SQL file.\n")
+sql("commit")
+close(fo)
