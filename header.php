01f642a3b079c0c8b4471dee42d19041a548819e
Nathanael Fillmore <github@nate-fillmore.com>
2014-10-14 22:27:36 -0500

    Cleaned up header.php, and included jquery here.

--- header.php
+++ header.php
@@ -1,39 +1,26 @@
 <?php
-ini_set('display_errors', "1");
+ini_set("display_errors", "1");
 error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
-<script src='DataTables-1.10.2/media/js/jquery.js'></script>
-<script src='DataTables-1.10.2/media/js/jquery.dataTables.min.js'></script>
-<script src='DataTables-1.10.2/extensions/Scroller/js/dataTables.scroller.min.js'></script>
+
+<!-- Data table includes -->
+<script src="DataTables-1.10.2/media/js/jquery.js"></script>
+<script src="DataTables-1.10.2/media/js/jquery.dataTables.min.js"></script>
+<script src="DataTables-1.10.2/extensions/Scroller/js/dataTables.scroller.min.js"></script>
 <link rel="stylesheet" type="text/css" href="DataTables-1.10.2/media/css/jquery.dataTables.css"></link>
 <link rel="stylesheet" type="text/css" href="DataTables-1.10.2/extensions/Scroller/css/dataTables.scroller.min.css"></link>
 
-<!-- <script src='http://www.biostat.wisc.edu/~nathanae/jquery/jquery-ui-1.10.3/jquery-1.9.1.js'></script> -->
-<script src='/~nathanae/jquery/jquery-ui-1.10.3/ui/minified/jquery-ui.min.js'></script>
-<script src='/~nathanae/jquery/jquery.layout-latest.js'></script>
-<link rel='stylesheet' type='text/css' href='/~nathanae/jquery/layout-default-latest.css' />
-<link rel='stylesheet' type='text/css' href='/~nathanae/jquery/jquery-ui-1.10.3/themes/base/minified/jquery-ui.min.css' />
+<!-- Layout includes -->
+<script src="jquery/jquery-ui-1.10.3/ui/minified/jquery-ui.min.js"></script>
+<script src="jquery/jquery.layout-latest.js"></script>
+<link rel="stylesheet" type="text/css" href="jquery/layout-default-latest.css"></link>
+<link rel="stylesheet" type="text/css" href="jquery/jquery-ui-1.10.3/themes/base/minified/jquery-ui.min.css"></link>
 
-<script type="text/x-mathjax-config">
-MathJax.Hub.Config({
-  tex2jax: {inlineMath: [['$','$']]}
-});
-</script>
-<script type="text/javascript"
-  src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_SVG">
-</script>
-
-<!--
-<link rel="stylesheet" href="canvasXpress.css" type="text/css"/>
-<script type="text/javascript" src="canvasXpress.min.js"></script>
--->
-
-<script type="text/javascript" src="d3.v3.min.js"></script>
-<script type="text/javascript" src="boxplot.js"></script>
-<link rel="stylesheet" href="boxplot.css" type="text/css"></link>
-
-<script type="text/javascript" src="jquery.fileDownload.js"></script>
+<!-- Boxplot includes -->
+<script src="d3.v3.min.js"></script>
+<script src="boxplot.js"></script>
+<link rel="stylesheet" type="text/css" href="boxplot.css"></link>
 
 <style>
 body, td { font-family: sans-serif; font-size: 9pt; }
